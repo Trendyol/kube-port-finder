8d8446829a27f663636bb6b93b1fdc117d9009a9
Baris Ceviz <me@barisceviz.com>
2019-06-24 03:38:34 +0300

    Implement Kubernetes API Client

---
+++ app/services/kubernetes-service.go
@@ -0,0 +1,47 @@
+package services
+
+import (
+	b64 "encoding/base64"
+	"github.com/imroc/req"
+	utils "github.com/peacecwz/kube-port-finder/app"
+	"github.com/peacecwz/kube-port-finder/app/models"
+)
+
+type KubeService struct {
+	AppName string
+	Ports   []int
+}
+
+type KubernetesService struct {
+}
+
+func (service KubernetesService) GetAllServices(host string, username string, password string) ([]KubeService, error) {
+	var result []KubeService
+	auth := b64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+	authHeader := req.Header{
+		"Accept":        "application/json",
+		"Authorization": "Basic " + auth,
+	}
+	var kubeServicesResponse = models.KubeServiceResult{}
+	err := utils.SendRequest(host+"/api/v1/services/", authHeader, &kubeServicesResponse)
+
+	if err != nil {
+		return result, err
+	}
+
+	for _, item := range kubeServicesResponse.Items {
+		var ports []int
+
+		for _, port := range item.Spec.Ports {
+			ports = append(ports, port.NodePort)
+		}
+
+		var kubeService = KubeService{
+			AppName: item.Metadata.Name,
+			Ports:   ports,
+		}
+		result = append(result, kubeService)
+	}
+
+	return result, err
+}
